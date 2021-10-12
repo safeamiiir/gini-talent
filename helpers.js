65eca5ddf722d23d209a61b300e7d7515681781f
AmirReza Safehian <amirreza.safehian@zoodfood.com>
2021-10-13 02:26:53 +0330

    fix(add-test): fix isInWeekRange functionality based on TDD

--- helpers.js
+++ helpers.js
@@ -14,21 +14,18 @@ function fetch(key) {
 function RoundUp(price, decimal) {
   return (
     Math.ceil(price * Math.pow(10, decimal)) / Math.pow(10, decimal)
-  ).toFixed(2);
+  ).toFixed(decimal);
 }
 
 function isInWeekRange(weekDate, date) {
-  const now = new Date(date);
-  const first = now.getDate() - now.getDay() + 1;
-  // First day is the day of the month - the day of the week
-  const last = first + 6;
-  // last day is the first day + 6
+  weekDate = new Date(weekDate);
+  date = new Date(date);
 
-  const firstDay = new Date(now.setDate(first)).getTime();
-  const lastDay = new Date(now.setDate(last)).getTime();
-  const operationDate = new Date(weekDate).getTime();
-
-  return firstDay <= operationDate && operationDate <= lastDay;
+  const day = weekDate.getDay();
+  const diff = weekDate.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+  const startOfTheWeek = new Date(weekDate.setDate(diff)).getTime();
+  const endOfTheWeek = new Date(weekDate.setDate(diff + 6)).getTime(); //end of the week is more 6 days
+  return startOfTheWeek <= date && date <= endOfTheWeek;
 }
 
 module.exports = { fetch, RoundUp, isInWeekRange };
